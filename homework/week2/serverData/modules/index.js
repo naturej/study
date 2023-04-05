7c7e1376c43466ecf30e640eaa88cd44fe9630a3
naturej <natu_rej@naver.com>
2023-04-05 23:37:05 +0900

    style: tailwind css 추가

--- homework/week2/serverData/modules/index.js
+++ homework/week2/serverData/modules/index.js
@@ -15,59 +15,79 @@ function authorInfo(authors) {
         author.id ? `/author/${author.id}` : `/search?q=${author.username}`
       }">${author.username}</a>`;
     })
-    .join();
-  return `<span>${str} ${etc > 0 ? `외 ${etc}명` : ``}</span>`;
+    .join(", ");
+  return `<span class="">${str} ${etc > 0 ? `외 ${etc}명` : ``}</span>`;
+}
+
+function priceComma(price) {
+  return price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
 
 function bookInfo(book) {
   return `
   <li>
       <div class="book" data-id="${book.id}">
-          <div class="thumbnail">
+        <div class="thumbnail-wrap">
+          <div class="thumbnail relative">
             <a href="${siteUrl}/books/${book.id}">
-                ${
-                  book.price.rentalPrice
-                    ? `<div class="badge rental">대여</div>`
-                    : `${
-                        book.price.discountPercent
-                          ? `<div class="badge discount">${book.price.discountPercent}%</div>`
-                          : ``
-                      }`
-                }
-                ${
-                  book.setBook
-                    ? `<div class="set_book">${book.setBook}권 세트</div>`
-                    : ``
-                }
-                <img src="${book.thumbnail.imgUrl}" width="110" alt="${
-    book.name
-  } 표지 이미지" />
-            </a>
+                  ${
+                    book.price.rentalPrice
+                      ? `<div class="absolute shadow-sm shadow-gray-500 -left-3 -top-3 w-11 h-11 flex justify-center items-center rounded-full text-white bg-emerald-600 badge">대여</div>`
+                      : `${
+                          book.price.discountPercent
+                            ? `<div class="absolute shadow-sm shadow-gray-500 -left-3 -top-3 w-11 h-11 flex justify-center items-center rounded-full text-white bg-slate-600 badge">${book.price.discountPercent}<span class="text-xs">%<span></div>`
+                            : ``
+                        }`
+                  }
+                  ${
+                    book.setBook
+                      ? `<div class="absolute bottom-12 left-0 right-0 shadow-sm shadow-gray-500 flex justify-center bg-white set-book">${book.setBook}권 세트</div>`
+                      : ``
+                  }
+                  <img class="shadow-sm shadow-gray-400" src="${
+                    book.thumbnail.imgUrl
+                  }" alt="${book.name} 표지 이미지" />
+              </a>
+            </div>
           </div>
           <div class="info">
-              <div class="title"><a href="${siteUrl}/books/${book.id}">${
-    book.title
-  }</a></div>
+              <div class="title mt-2 text-sm tracking-tight line-clamp-2"><a class="hover:underline underline-offset-1" href="${siteUrl}/books/${
+    book.id
+  }">${book.title}</a></div>
+              <div class="author text-slate-500 text-xs mt-1">${authorInfo(
+                book.author
+              )}</div>
               <div class="starRate">
-                  <div style="width:${book.starRate.score}%"></div>
-                  <div class="participant_count">${
-                    book.starRate.participantCount
-                  }명</div>
+                <span class="StarRate_IconBox">
+                  <span class="StarRate_IconFill" style="width:${
+                    book.starRate.score
+                  }%"></span>
+                </span>
+                ${
+                  book.starRate.participantCount > 0
+                    ? `<span class="participant_count text-xs font-light text-slate-400">${book.starRate.participantCount}명</span>`
+                    : ``
+                }
               </div>
-              <div class="author">${authorInfo(book.author)}</div>
-              <div class="publisher"><a href="${siteUrl}/search?q=${
+              <div class="publisher hidden"><a class="text-slate-500 text-xs" href="${siteUrl}/search?q=${
     book.publisher
   }">${book.publisher}</a></div>
-              <div class="genre">${book.genre}</div>
+              <div class="genre text-slate-500 text-xs hidden">${
+                book.genre
+              }</div>
               <div class="price">
                   ${
                     book.price.rentalPrice
-                      ? `<div class="rental_price">대여 : ${book.price.rentalPrice}원</div>`
+                      ? `<div class="rental_price text-slate-500 text-xs">대여 <span class="text-sky-600 font-medium">${priceComma(
+                          book.price.rentalPrice
+                        )}원</span></div>`
                       : ``
                   }
-                  <div class="buy_price">소장 : ${book.price.buyPrice}원${
+                  <div class="buy_price text-slate-500 text-xs">소장 <span class="text-sky-600 font-medium">${priceComma(
+                    book.price.buyPrice
+                  )}원</span>${
     book.price.discountPercent
-      ? `<span class="discount">(${book.price.discountPercent}%↓)</span>`
+      ? `<span class="discount text-red-500">&nbsp;(${book.price.discountPercent}%↓)</span>`
       : ``
   }</div>
               </div>
