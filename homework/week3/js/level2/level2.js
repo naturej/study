805f9ef0f00a35e06c5059e21acea4150f0caf36
naturejj <nature.j@foodnamoo.com>
2023-04-12 18:11:54 +0900

    feat: js level2 문제 푸는중 중간저장

--- homework/week3/js/level2/level2.js
+++ homework/week3/js/level2/level2.js
@@ -6,3 +6,24 @@ console.log(RESERVATION_LIST);
 
 
 */
+
+const userName = document.getElementsByName("user-name")[0].value;
+const userPhone = document.getElementsByName("user-phone")[0].value;
+const $btnSubmit = document.getElementById("btn-submit");
+const $reservation = document.getElementById("reservation-number");
+
+$btnSubmit.addEventListener("click",(event)=>{
+    event.preventDefault();
+
+    const customer = RESERVATION_LIST.find((el, index)=>{
+        return el.name === userName && el.phone === userPhone;
+    });
+    console.log(customer);
+
+    if(!customer) {
+        alert("일치하는 내역이 없습니다.");
+    } else {
+        $reservation.innerText = customer.number;
+    }
+
+});
