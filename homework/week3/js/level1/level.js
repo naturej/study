0183863ffbbc568c997e53261d91e86e20e6125b
naturejj <nature.j@foodnamoo.com>
2023-04-13 08:59:10 +0900

    refactor: js lv1 함수 수정
    formatBankNumber 함수 로직 수정

--- homework/week3/js/level1/level.js
+++ homework/week3/js/level1/level.js
@@ -4,7 +4,7 @@ import { BANK_LIST, ACCOUNT_FORM } from "./account.js";
 // console.log(ACCOUNT_FORM);
 
 /*
-    계좌 번호 파싱 하기
+  계좌 번호 파싱 하기
 */
 
 const $bankSelector = document.getElementById("bank-selector");
@@ -29,38 +29,43 @@ $bankSelector.options[0].selected = true;
 // 제출 버튼 클릭 시
 $btnSubmit.addEventListener("click", (event) => {
   event.preventDefault();
-  if ($inputAccount.value.length < 12) return alert("12자리를 입력해주세요");
+  const str = $inputAccount.value;
 
-  let str = $inputAccount.value; // str : 123412341234 형식
-  str = str.substring(0, 2) + "*".repeat(8) + str.substring(10); // 12********34 형식으로 변경
+  if (str.length < 12) return alert("12자리를 입력해주세요");
 
-  const maskStr = formatBankNumber(str); // 파싱
+  const bankNumStr = formatBankNumber(str);
 
   const $li = document.createElement("li");
   $li.innerText = `${
     $bankSelector[$bankSelector.selectedIndex].innerText
-  } : ${maskStr}`;
+  } : ${bankNumStr}`;
 
   $accountList.appendChild($li);
 });
 
 /**
- * 문자열을 입력받아 선택된 은행의 계좌 번호 형식으로 파싱하여 반환하는 함수
+ * 문자열을 입력받아 앞, 뒤 2자리 뺀 나머지 자리 마스킹 처리(*) 하고
+ * 선택된 은행의 계좌 번호 형식으로 파싱하여 반환하는 함수
  * @param {string} str
- * @returns {string} maskStr
+ * @returns {string} bankNumStr
  */
 function formatBankNumber(str) {
   const format = $bankSelector[$bankSelector.selectedIndex].dataset.format;
   const formatArr = format.split("-");
   let curIndex = 0;
 
-  const maskStr = formatArr
+  // str : 123412341234 형식
+  // maskStr : 12********34 형식으로 변경
+  const maskStr = str.substring(0, 2) + "*".repeat(8) + str.substring(10);
+
+  // bankNumStr : 12-********-34 형식으로 변경
+  const bankNumStr = formatArr
     .map((el) => {
-      const newStr = str.substring(curIndex, curIndex + el.length);
+      const result = maskStr.substring(curIndex, curIndex + el.length);
       curIndex += el.length;
-      return newStr;
+      return result;
     })
     .join("-");
 
-  return maskStr;
+  return bankNumStr;
 }
