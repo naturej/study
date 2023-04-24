dd492fe241b9d2cc30509f2ab155495e0dec4023
naturej <natu_rej@naver.com>
2023-04-24 23:00:40 +0900

    chore: 랜덤 이미지 0~3개 노출로 수정

--- homework/week4/faker/faker.js
+++ homework/week4/faker/faker.js
@@ -18,7 +18,7 @@ export const MockPosts = (count) =>
         nickName: faker.name.firstName(),
         profileImg: faker.image.avatar(),
       },
-      Post_img: Array(Math.floor(Math.random() * 3) + 0)
+      Post_img: Array(Math.floor(Math.random() * 4) + 0)
         .fill()
         .map(() => faker.image.image()),
       Comments: Array(Math.floor(Math.random() * 5) + 1)
